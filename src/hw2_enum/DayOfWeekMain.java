3b597d3cb57204f5728e387301c5b28cbf122292
Evgenia <zhenja_a@bk.ru>
2021-01-30 20:53:59 +0300

    Выполненное дз

--- src/hw2_enum/DayOfWeekMain.java
+++ src/hw2_enum/DayOfWeekMain.java
@@ -1,5 +1,17 @@
 package hw2_enum;
 
 public class DayOfWeekMain {
+    private static int workingHoursADay = 8;
 
+    public static void main(String[] args) {
+        System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+    }
+
+    public static String getWorkingHours(DayOfWeek p){
+        if (p.ordinal() == 5 || p.ordinal() == 6){
+            return("Сегодня выходной!");
+        } else {
+            return Integer.toString(DayOfWeek.SATURDAY.compareTo(p) * workingHoursADay);
+        }
+    }
 }
